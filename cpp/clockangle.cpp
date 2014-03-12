7fc79b404458f52deba12e072f7d5a0548c7f799
Pearce Keesling <keeslinp@gmail.com>
2014-03-12 17:12:18 +0100

    finished the clockangle program

--- cpp/clockangle.cpp
+++ cpp/clockangle.cpp
@@ -7,26 +7,26 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    string buff(argv[1]);
-    string hour("");
-    string min("");
-    string *target = &hour;
-    for(string::iterator it = buff.begin();it<buff.end();it++)
-    {
-        if(*it==':' || *it=='h')
-        {
-            it++;
-            target = &min;
-        }
-        *target += *it;
-    }
-     
-    float h,m;
-    stringstream(hour) >> h;
-    stringstream(min) >> m;
-     
-    int answer = fabs((h/24 - m/60) * 360);
-    //if(answer > 180) answer-=180;
-    cout << answer;
-    return 0;
+	string buff(argv[1]);
+	string hour("");
+	string min("");
+	string *target = &hour;
+	for(string::iterator it = buff.begin();it<buff.end();it++)
+	{
+		if(*it==':' || *it=='h')
+		{
+			it++;
+			target = &min;
+	        }
+	        *target += *it;
+	}
+	    
+	float h,m;
+	stringstream(hour) >> h;
+	stringstream(min) >> m;
+//	if(h > 12) h-=12;
+	float answer = fabs(((h+m/60)/24 - m/60) * 360);
+	if(answer > 180) answer = 360 - answer;
+	cout << answer << endl;
+	return 0;
 }
